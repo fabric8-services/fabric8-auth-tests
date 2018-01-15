a2b8b01803fe122c4f440bc5ebea6873216eb7b9
Pavel Macík <pavel.macik@gmail.com>
2018-01-15 11:38:51 +0100

    [performance] auth-api-user: fixed run.sh for slaves/tokens distribution

--- performance/scripts/auth-api-user/run.sh
+++ performance/scripts/auth-api-user/run.sh
@@ -29,15 +29,27 @@ TOKEN_COUNT=`cat $TOKENS_FILE | wc -l`
 i=1
 s=1
 rm -rf $TOKENS_FILE-slave-*;
-while [ $i -le $TOKEN_COUNT ]; do
-	sed "${i}q;d" $TOKENS_FILE >> $TOKENS_FILE-slave-$s;
-	i=$((i+1));
-	if [ $s -lt $SLAVES ]; then
+if [ $TOKEN_COUNT -ge $SLAVES ]; then
+	while [ $i -le $TOKEN_COUNT ]; do
+		sed "${i}q;d" $TOKENS_FILE >> $TOKENS_FILE-slave-$s;
+		i=$((i+1));
+		if [ $s -lt $SLAVES ]; then
+			s=$((s+1));
+		else
+			s=1;
+		fi;
+	done;
+else
+	while [ $s -le $SLAVES ]; do
+		sed "${i}q;d" $TOKENS_FILE >> $TOKENS_FILE-slave-$s;
 		s=$((s+1));
-	else
-		s=1;
-	fi;
-done
+		if [ $i -lt $TOKEN_COUNT ]; then
+			i=$((i+1));
+		else
+			i=1;
+		fi;
+	done;
+fi
 
 for s in $(seq 1 $SLAVES); do
 	echo "#!/bin/bash
