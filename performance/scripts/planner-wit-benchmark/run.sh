883a53c6bd0bd7d7d1a26d33e1324d9e7fb3282c
Pavel Macík <pavel.macik@gmail.com>
2018-02-08 12:35:29 +0100

    [performance] planner-wit-benchmark: Added Zabbix report

--- performance/scripts/planner-wit-benchmark/run.sh
+++ performance/scripts/planner-wit-benchmark/run.sh
@@ -3,10 +3,20 @@
 source _setenv.sh
 
 echo " Prepare environment"
-./prepare.sh
+./_prepare.sh
 
-echo "Build WIT server"
-./build.sh
+echo " Build WIT server"
+./_build.sh
 
-echo "Run tests"
-./test.sh
+echo " Run tests"
+./_test.sh
+
+echo " Prepare Zabbix report"
+export ZABBIX_LOG=$JOB_BASE_NAME-$BUILD_NUMBER-zabbix.log
+./_zabbix.sh $JOB_BASE_NAME-$BUILD_NUMBER-results.csv $ZABBIX_LOG
+
+if [[ "$ZABBIX_REPORT_ENABLED" = "true" ]]; then
+	echo "  Uploading report to zabbix...";
+	zabbix_sender -vv -i $ZABBIX_LOG -T -z $ZABBIX_SERVER -p $ZABBIX_PORT;
+fi
+
